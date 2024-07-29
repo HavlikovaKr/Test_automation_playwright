f3034108d10ae50068bf8e46b4ed987645d7a494
Kristýna Havlíková <kr.havlikova17@gmail.com>
2024-07-29 13:52:31 +0200

    Přidání knihoven a pytest.fixture

---
+++ test_web_auto.py
@@ -0,0 +1,18 @@
+# import page
+import pytest
+from playwright.sync_api import sync_playwright
+
+
+@pytest.fixture()
+def browser():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=True, slow_mo=500)
+        yield browser
+        browser.close()
+
+
+@pytest.fixture()
+def page(browser):
+    page = browser.new_page()
+    yield page
+    page.close()
